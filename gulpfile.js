511b23e2b2def7213bc9db61a190aaa7a9646739
Grinvald Anastassiya <grinvald.anastassiya@gmail.com>
2019-08-17 12:02:08 +0600

    1

--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,10 @@ var path = {
   src: {
     html: 'assets/src/*.html',
     js: ['assets/src/js/index.js', 'assets/src/js/akademicheskaya.js', 
-    'assets/src/js/kids.js', 'assets/src/js/kupchino.js', 'assets/src/js/komendanskiy.js' ],
+    'assets/src/js/kids.js', 'assets/src/js/kupchino.js', 'assets/src/js/komendanskiy.js', 'assets/src/js/promotions.js' ],
     style: ['assets/src/style/index.scss', 'assets/src/style/akademicheskaya.scss',
     'assets/src/style/komendanskiy.scss', 'assets/src/style/kupchino.scss', 
-    'assets/src/style/kids.scss'],
+    'assets/src/style/kids.scss', 'assets/src/style/promotions.scss'],
     img: 'assets/src/img/**/*.*',
     fonts: 'assets/src/fonts/**/*.*'
   },
