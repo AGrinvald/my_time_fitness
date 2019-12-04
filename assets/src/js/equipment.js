71061625bb7e1dce4b69191efa1f265440cb633b
Grinvald Anastassiya <grinvald.anastassiya@gmail.com>
2019-12-04 12:06:39 +0600

    1

---
+++ assets/src/js/equipment.js
@@ -0,0 +1,60 @@
+// импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// импортируем Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// импортируем необходимые js-файлы Bootstrap 4
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/alert.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+//= ../../../node_modules/bootstrap/js/dist/toast.js
+
+// импортируем jQuery Masked Input
+//= ../../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
+
+function bossModalNextClick() {
+
+    var name = document.getElementById("bossContactName"); 
+    var phone = document.getElementById("bossContactPhone"); 
+    var email = document.getElementById("bossContactEmail"); 
+    var message = document.getElementById("bossContactMessage"); 
+
+    if (!name.checkValidity()) {
+        name.parentElement.setAttribute("style", "background-color: #FFDBDC");
+    }
+
+    if (!phone.checkValidity()) {
+        phone.parentElement.setAttribute("style", "background-color: #FFDBDC");
+    }
+
+    if (!email.checkValidity()) {
+        email.parentElement.setAttribute("style", "background-color: #FFDBDC");
+    }
+
+    if (name.checkValidity() && phone.checkValidity() && email.checkValidity()) {
+        var next = $(this).data("next");
+
+        $.ajax({
+            url: next,
+            dataType: 'html'
+        }).done(function (html) {
+            $("#contact-boss-modal .modal-body").html(html);
+        });
+    }
+}
+
+$(function () {
+    $('#contact-boss-modal').on('shown.bs.modal', function (e) {
+        $("#contact-boss-modal .promo-btn").bind("click", bossModalNextClick);
+        $("#bossContactPhone").mask("+7(999) 999-99-99");
+    });
+});
